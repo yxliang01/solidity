c6a69bf689c1cd2f16b3f9c0f3b88c3a10af5da4
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-06-15 17:11:26 +0200

    Yul proto fuzzer: Make function definition a statement

--- test/tools/ossfuzz/protoToYul.h
+++ test/tools/ossfuzz/protoToYul.h
@@ -40,12 +40,12 @@ class ProtoConverter
 public:
 	ProtoConverter()
 	{
-		m_numFunctionSets = 0;
 		m_inForBodyScope = false;
 		m_inForInitScope = false;
 		m_numNestedForLoops = 0;
 		m_counter = 0;
 		m_inputSize = 0;
+		m_inFunctionDef = false;
 	}
 	ProtoConverter(ProtoConverter const&) = delete;
 	ProtoConverter(ProtoConverter&&) = delete;
@@ -54,21 +54,16 @@ public:
 private:
 	void visit(BinaryOp const&);
 	void visit(Block const&, std::vector<std::string>&& = {});
-	void visit(SpecialBlock const&, std::vector<std::string>&& = {});
 	std::string visit(Literal const&);
 	void visit(VarRef const&);
 	void visit(Expression const&);
 	void visit(VarDecl const&);
-	void visit(EmptyVarDecl const&);
-	void visit(MultiVarDecl const&);
 	void visit(TypedVarDecl const&);
 	void visit(UnaryOp const&);
 	void visit(AssignmentStatement const&);
-	void visit(MultiAssignment const&);
 	void visit(IfStmt const&);
 	void visit(StoreFunc const&);
 	void visit(Statement const&);
-	void visit(FunctionDefinition const&);
 	void visit(ForStmt const&);
 	void visit(BoundedForStmt const&);
 	void visit(CaseStmt const&);
@@ -82,18 +77,12 @@ private:
 	void visit(RetRevStmt const&);
 	void visit(SelfDestructStmt const&);
 	void visit(TerminatingStmt const&);
-	void visit(FunctionCallNoReturnVal const&);
-	void visit(FunctionCallSingleReturnVal const&);
 	void visit(FunctionCall const&);
-	void visit(FunctionDefinitionNoReturnVal const&);
-	void visit(FunctionDefinitionSingleReturnVal const&);
-	void visit(FunctionDefinitionMultiReturnVal const&);
+	void visit(FunctionDef const&);
 	void visit(Program const&);
-	void registerFunction(FunctionDefinition const&);
 	void openScope(std::vector<std::string>&& _x);
 	void closeScope();
-	void addToScope(std::vector<std::string>&&);
-
+	void addVarsToScope(std::vector<std::string>&&);
 	std::string createHex(std::string const& _hexBytes);
 
 	/// Accepts an arbitrary string, removes all characters that are neither
@@ -106,33 +95,56 @@ private:
 		Multiple
 	};
 
-	template<class T>
-	void visitFunctionInputParams(T const&, unsigned);
+	void visitFunctionInputParams(FunctionCall const&, unsigned);
+	void createFunctionDefAndCall(FunctionDef const&, unsigned, unsigned);
 
-	template<class T>
-	void createFunctionDefAndCall(T const&, unsigned, unsigned, NumFunctionReturns);
+	/// Convert function type to a string to be used while naming a
+	/// function that is created by a function declaration statement.
+	/// @param _type Type classified according to the number of
+	/// values returned by function.
+	/// @return A string as follows. If _type is
+	/// None -> "n"
+	/// Single -> "s"
+	/// Multiple -> "m"
 	std::string functionTypeToString(NumFunctionReturns _type);
+
+	/// Return true if at least one variable declaration is in scope,
+	/// false otherwise.
+	/// @return True in the following cases:
+	/// - If we are inside a function that has already declared a variable
+	/// - If there is at least one variable declaration that is
+	/// in scope
+	bool varDeclAvailable();
+
+	/// Return true if a function call cannot be made, false otherwise.
+	/// @param _type is an enum denoting the type of function call. It
+	/// can be one of NONE, SINGLE, MULTIDECL, MULTIASSIGN.
+	/// 			NONE -> Function call does not return a value
+	///				SINGLE -> Function call returns a single value
+	///				MULTIDECL -> Function call returns more than one value
+	///					and it is used to create a multi declaration
+	///					statement
+	///				MULTIASSIGN -> Function call returns more than one value
+	///					and it is used to create a multi assignment
+	///					statement
+	/// @return True if the function call cannot be created for one of the
+	/// following reasons
+	//   - It is a SINGLE function call (we reserve SINGLE functions for
+	//   expressions)
+	//   - It is a MULTIASSIGN function call and we do not have any
+	//   variables available for assignment.
+	bool functionCallNotPossible(FunctionCall_Returns _type);
 
 	/// Creates variable declarations "x_<_startIdx>",...,"x_<_endIdx - 1>"
 	std::vector<std::string> createVars(unsigned _startIdx, unsigned _endIdx);
 
-	template <class T>
-	void registerFunction(T const& _x, NumFunctionReturns _type, unsigned _numOutputParams = 0)
-	{
-		unsigned numInputParams = _x.num_input_params() % s_modInputParams;
-		switch (_type)
-		{
-			case NumFunctionReturns::None:
-				m_functionVecNoReturnValue.push_back(numInputParams);
-				break;
-			case NumFunctionReturns::Single:
-				m_functionVecSingleReturnValue.push_back(numInputParams);
-				break;
-			case NumFunctionReturns::Multiple:
-				m_functionVecMultiReturnValue.push_back(std::make_pair(numInputParams, _numOutputParams));
-				break;
-		}
-	}
+	/// Register a function declaration
+	/// @param _f Pointer to a FunctionDef object
+	void registerFunction(FunctionDef const* _f);
+
+	/// Removes entry from m_functionMap and m_functionName
+	void updateFunctionMaps(std::string _x);
+
 	/// Returns a pseudo-random dictionary token.
 	/// @param _p Enum that decides if the returned token is hex prefixed ("0x") or not
 	/// @return Dictionary token at the index computed using a
@@ -148,33 +160,46 @@ private:
 		return m_counter++;
 	}
 
+	/// Generate function name of the form "foo_<typeSuffix>_<counter>".
+	/// @param _type Type classified according to the number of
+	/// values returned by function.
+	std::string functionName(NumFunctionReturns _type)
+	{
+		return "foo_" + functionTypeToString(_type) + "_" + std::to_string(counter());
+	}
+
 	std::ostringstream m_output;
-	/// Scope
-	std::stack<std::set<std::string>> m_scopes;
+	/// Variables in current scope
+	std::stack<std::vector<std::string>> m_scopeVars;
+	/// Functions in current scope
+	std::stack<std::vector<std::string>> m_scopeFuncs;
 	/// Variables
 	std::vector<std::string> m_variables;
+	/// Functions
+	std::vector<std::string> m_functions;
+	/// Maps FunctionDef object to its name
+	std::map<FunctionDef const*, std::string> m_functionDefMap;
 	// Set that is used for deduplicating switch case literals
 	std::stack<std::set<dev::u256>> m_switchLiteralSetPerScope;
-	// Total number of function sets. A function set contains one function of each type defined by
-	// NumFunctionReturns
-	unsigned m_numFunctionSets;
 	// Look-up table per function type that holds the number of input (output) function parameters
-	std::vector<unsigned> m_functionVecNoReturnValue;
-	std::vector<unsigned> m_functionVecSingleReturnValue;
-	std::vector<std::pair<unsigned, unsigned>> m_functionVecMultiReturnValue;
+	std::map<std::string, std::pair<unsigned, unsigned>> m_functionSigMap;
 	// mod input/output parameters impose an upper bound on the number of input/output parameters a function may have.
 	static unsigned constexpr s_modInputParams = 5;
 	static unsigned constexpr s_modOutputParams = 5;
-	// predicate to keep track of for body scope
+	/// Predicate to keep track of for body scope. If true, break/continue
+	/// statements can not be created.
 	bool m_inForBodyScope;
 	// Index used for naming loop variable of bounded for loops
 	unsigned m_numNestedForLoops;
-	// predicate to keep track of for loop init scope
+	/// Predicate to keep track of for loop init scope. If true, variable
+	/// or function declarations can not be created.
 	bool m_inForInitScope;
 	/// Monotonically increasing counter
 	unsigned m_counter;
 	/// Size of protobuf input
 	unsigned m_inputSize;
+	/// Predicate that is true if inside function definition, false otherwise
+	bool m_inFunctionDef;
 };
 }
 }
